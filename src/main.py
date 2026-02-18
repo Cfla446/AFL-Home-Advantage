75e28016aa3cb8d735b8b6f4aea4d8fc4bc61c2d
Cormac <cormac@LAPTOP-0C8K460R.localdomain>
2026-02-18 23:33:26 +1100

    feat: add simple main.py implementation

---
+++ src/main.py
@@ -0,0 +1,11 @@
+from load_data import main as load_data_main
+from analysis import main as analysis_main
+
+
+def main():
+    load_data_main()
+    analysis_main()
+
+
+if __name__ == "__main__":
+    main()
